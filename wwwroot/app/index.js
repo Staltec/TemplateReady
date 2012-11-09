91cffdf568beb27102adfaa7054a8e198f1ddb1e
Alexander "Staltec" Prozorov <staltec@gmail.com>
2012-11-09 23:13:28 +0800

    Change test code

--- wwwroot/app/index.js
+++ wwwroot/app/index.js
@@ -2,14 +2,7 @@
 * Template ready test
 */
 
-var Core = {
-
-   Model: {},
-   Collection: {},
-   View: {},
-   Template: {},
-
-   testData: {
+   var testData = {
       title:'Тестовый заголовок',
       list: ['Один', 'Два', 'Три'],
       hash: {firstName:'Майкл', lastName:'Джексон', age:'RIP'},
@@ -21,34 +14,36 @@ var Core = {
          { year: 1979, album: "«Off the Wall»" },
          { year: 1982, album: "«Thriller»" }
       ]
-   },
+   }
 
-   runTests: function(){
-      Core.Template = TemplateReady;
+   function runTests (){
+      Template = TemplateReady;
 
       // Underscore template engine
-      Core.testFunc('underscoreTest');
-      Core.testFile('underscore/test.htm');
+      testFunc('underscoreTest');
+      testFile('underscore/test.htm');
 
       // Mustache template engine
-      Core.testFunc('mustacheTest');
-      Core.testFile('mustache/test.mustache');
+      testFunc('mustacheTest');
+      testFile('mustache/test.mustache');
 
       // Mustache template with space in filename
-      Core.testFunc('mustacheTest_space');
-      Core.testFile('mustache/test space.mustache');
+      testFunc('mustacheTest_space');
+      testFile('mustache/test space.mustache');
 
-   },
+   }
 
    /* ----------------------------------------------- */
 
-   testFunc: function(funcName, locals){
-      Core.counter = 1+Core.counter || 0;
-      if(!locals) locals = Core.testData;
+   var Template, counter = 0;
+
+   function testFunc(funcName, locals){
+      counter++;
+      if(!locals) locals = testData;
 
       var html, err;
       try{
-         html = Core.Template[funcName] ? Core.Template[funcName](locals) : (err=true, 'not found');
+         html = Template[funcName] ? Template[funcName](locals) : (err=true, 'not found');
       }catch(e){
          err = true;
          html = e;
@@ -56,31 +51,30 @@ var Core = {
       }
 
       var sb = document.getElementById('statusBar');
-      sb.innerHTML += '<a href="#'+Core.counter+'" style="text-decoration:none; background-color:'+(err ? 'red' : 'green')+'">&nbsp;&nbsp;&nbsp;&nbsp;</a>&nbsp;';
+      sb.innerHTML += '<a href="#'+counter+'" style="text-decoration:none; background-color:'+(err ? 'red' : 'green')+'">&nbsp;&nbsp;&nbsp;&nbsp;</a>&nbsp;';
 
       var tests = document.getElementById('tests');
-      tests.innerHTML +='<span id="'+Core.counter+'" style="background-color:'+(err ? 'red' : 'green')+'">&nbsp;&nbsp;&nbsp;&nbsp;</span> <strong>Function "'+funcName+'"</strong>';
+      tests.innerHTML +='<span id="'+counter+'" style="background-color:'+(err ? 'red' : 'green')+'">&nbsp;&nbsp;&nbsp;&nbsp;</span> <strong>Function "'+funcName+'"</strong>';
       tests.innerHTML +='<div style="padding:5px; margin-bottom:30px; border: 1px dashed #999999; background-color: #eeeeee">'+html+'</div>';
-   },
+   }
 
-   testFile: function(fileName, locals){
-      Core.counter = 1+Core.counter || 0;
-      if(!locals) locals = Core.testData;
+   function testFile(fileName, locals){
+      counter++;
+      if(!locals) locals = testData;
 
       var sb = document.getElementById('statusBar');
-      sb.innerHTML += '<a href="#'+Core.counter+'" style="text-decoration:none; background-color:'+(err ? 'red' : 'green')+'">&nbsp;&nbsp;&nbsp;&nbsp;</a>&nbsp;';
+      sb.innerHTML += '<a href="#'+counter+'" style="text-decoration:none; background-color:'+(err ? 'red' : 'green')+'">&nbsp;&nbsp;&nbsp;&nbsp;</a>&nbsp;';
 
       var html, err;
       try{
-         html = Core.Template._names[fileName] ? Core.Template.require(fileName)(locals) : (err=true, 'not found');
+         html = Template._names[fileName] ? Template.require(fileName)(locals) : (err=true, 'not found');
       }catch(e){
          err = true;
          html = e;
       }
 
       var tests = document.getElementById('tests');
-      tests.innerHTML += '<span id="'+Core.counter+'" style="background-color:'+(err ? 'red' : 'green')+'">&nbsp;&nbsp;&nbsp;&nbsp;</span> <strong>File "'+fileName+'"</strong>';
+      tests.innerHTML += '<span id="'+counter+'" style="background-color:'+(err ? 'red' : 'green')+'">&nbsp;&nbsp;&nbsp;&nbsp;</span> <strong>File "'+fileName+'"</strong>';
       tests.innerHTML += '<div style="padding:5px; margin-bottom:30px; border: 1px dashed #999999; background-color: #eeeeee">'+html+'</div>';
    }
-
-};
+   
